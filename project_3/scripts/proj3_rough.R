db5abe6c3071e772016984702fbbb6e65f952d8d
kingwilliam87 <54555563+kingwilliam87@users.noreply.github.com>
2019-12-01 15:56:54 -0500

    proj3 final

--- project_3/scripts/proj3_rough.R
+++ project_3/scripts/proj3_rough.R
@@ -5,10 +5,20 @@ library(ggplot2)
 library(dplyr)
 library(lubridate)
 
+install.packages('zoo')
 #Load csv file created in Pandas
-umd<-read_csv('proj3_umd.csv')
+umd<-read_csv('data/proj3_umd.csv')
 
 view(umd)
+
+ee<-read_csv('data/proj3_ee.csv')
+
+view(ee)
+
+ee.count
+
+ggplot(ee, aes(x=Duration))+
+  geom_histogram(aes(fill=`Reason for Leaving`))
 
 #Plot information about clients
 #Recent months homeless by gender
@@ -18,16 +28,36 @@ ggplot(umd, aes(x=Recent_Months_Homeless))+geom_bar(aes(fill=Gender))
 ggplot(umd, aes(x=Recent_Months_Homeless))+geom_bar(aes(fill=Race))
 
 #Histogram of entry age and gender
-ggplot(umd, aes(x=Entry_Age))+geom_histogram(aes(fill=Gender))
+ggplot(umd, aes(x=Entry_Age))+
+  geom_histogram(aes(fill=Gender))+
+  labs(x='Entry Age',y='Number of Clients')
 
 #Horizontal bar of previous living situation by previous times homeless
-ggplot(umd, aes(x=Prior_Living)) +geom_bar(aes(fill=Recent_Times_Homeless))+coord_flip()
+ggplot(umd, aes(x=Prior_Living)) +
+  geom_bar(aes(fill=Recent_Times_Homeless))+
+  coord_flip()+
+  labs(y='Number of Clients',x='Prior Living Situation',fill='Recent Times Homeless')
 
 #Look more closely at most common previous living situations
 umd_living <- umd %>%
   filter(Prior_Living =='Place not meant for habitation' | Prior_Living =='Emergency shelter' | Prior_Living =='Family Home' | Prior_Living =='Friend Home')
 
-ggplot(umd_living, aes(x=Prior_Living))+geom_bar(aes(fill=Prior_Living_Time))
+ggplot(umd_living, aes(x=Prior_Living))+
+  geom_bar(aes(fill=Prior_Living_Time))+
+  labs(x='Prior Living Situation',y='Number of Clients',fill='Recent Duration Homeless')
+
+dis<-read_csv('data/proj3_dis.csv')
+
+view(dis)
+
+view(umd_living_head)
+
+?theme
+
+ggplot(dis, aes(x=Disability_Type))+
+  geom_bar()+
+  labs(x='Disability Type',y='Number of Clients')+
+  coord_flip()
 
 #These are plots provide some basic information about clients
 
